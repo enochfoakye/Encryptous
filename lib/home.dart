e82084b056be6e1bc3becce96e96b0402dd8b81a
enochfoakye <enochfoakye@gmail.com>
2023-04-07 23:44:05 +0100

    Added Bottom Navbar and Stored Card viewer

--- lib/home.dart
+++ lib/home.dart
@@ -3,8 +3,10 @@ import 'package:my_app/add_new_card.dart';
 import 'package:my_app/database.dart';
 import 'package:encrypt/encrypt.dart' as encrypt;
 import 'passcode.dart';
-
 import 'env.dart' as env;
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+
+import 'tester.dart';
 
 class HomePage extends StatefulWidget {
   static const routeName = '/Encryptous';
@@ -14,102 +16,142 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-//
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Chnage Passcode',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('HomePage',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('NewCard',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+  ];
+  @override
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  List<Map<String, dynamic>> _cards = [];
+  void initState() {
+    super.initState();
+    // _showCards(); // you are here
+  }
+
+  Future<void> _loadCards() async {
+    final cards = await EncryptousHelper.instance.queryAllRows();
+    setState(() {
+      _cards = cards;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: const Text('Encryptous'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                print('Hello Zaddy');
-              },
-              child: const Text('to Login'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, NewCardPage.routeName);
-              },
-              child: const Text('to add Card'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, LoginPage.routeName
-
-                    //This is for the button pressed to navigate to the login page from home
-                    );
-              },
-              child: const Text('to add User'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                final rows = await EncryptousHelper.instance.queryAllRows();
-                final key = encrypt.Key.fromUtf8(
-                    env.aes_private_key); // Make this global
-                final iv = encrypt.IV.fromLength(16);
-                final encrypter = encrypt.Encrypter(encrypt.AES(key));
-
-                for (final row in rows) {
-                  print(row);
-                  try {
-                    final cardNumber =
-                        encrypter.decrypt64(row['card_number'], iv: iv);
-                    final expiryDate =
-                        encrypter.decrypt64(row['expiry_date'], iv: iv);
-                    final cardHolderName =
-                        encrypter.decrypt64(row['card_holder_name'], iv: iv);
-                    final cvvCode =
-                        encrypter.decrypt64(row['cvv_code'], iv: iv);
-
-                    print('Card Number: $cardNumber');
-                    print('Expiry Date: $expiryDate');
-                    print('Card Holder Name: $cardHolderName');
-                    print('CVV Code: $cvvCode');
-                    print('-------------------');
-                  } catch (e) {
-                    final cardNumber = "ERROR: $e";
-                    final expiryDate = "ERROR: $e";
-                    final cardHolderName = "ERROR: $e";
-                    final cvvCode = "ERROR: $e";
-                    print('Card Number: $cardNumber');
-                    print('Expiry Date: $expiryDate');
-                    print('Card Holder Name: $cardHolderName');
-                    print('CVV Code: $cvvCode');
-                    print('-------------------');
-                  }
-                }
-              },
-              child: const Text('Print all data'),
+        child: mainMenu(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.password),
+                label: 'Change Password',
+                backgroundColor: Colors.blue),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.credit_card),
+                label: 'Home',
+                backgroundColor: Colors.red),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: 'Add New Card',
+              backgroundColor: Colors.blue,
             ),
           ],
-        ),
-      ),
+          type: BottomNavigationBarType.shifting,
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.black,
+          iconSize: 40,
+          onTap: _onItemTapped,
+          elevation: 5),
     );
   }
+
+  Widget mainMenu(index) {
+    if (index == 0) {
+      return Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              print('Hello ');
+            },
+            child: const Text('to Login'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, LoginPage.routeName);
+              //This is for the button pressed to navigate to the login page from home
+            },
+            child: const Text('to add User'),
+          ),
+        ],
+      );
+    } else if (index == 1) {
+      // Middle button
+      return Scaffold(
+        body: _cards.isEmpty
+            ? const Center(
+                child: Text('No cards found'),
+              )
+            : ListView.builder(
+                itemCount: _cards.length,
+                itemBuilder: (context, index) {
+                  print(index);
+                  final card = _cards[index];
+                  final key = encrypt.Key.fromUtf8(env.aes_private_key);
+                  final iv = encrypt.IV.fromLength(16);
+                  final encrypter = encrypt.Encrypter(encrypt.AES(key));
+                  final cardNumber =
+                      encrypter.decrypt64(card['card_number'], iv: iv);
+                  final expiryDate =
+                      encrypter.decrypt64(card['expiry_date'], iv: iv);
+                  final cardHolderName =
+                      encrypter.decrypt64(card['card_holder_name'], iv: iv);
+                  final cvvCode = encrypter.decrypt64(card['cvv_code'], iv: iv);
+
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: CreditCardWidget(
+                      cardNumber: cardNumber,
+                      expiryDate: expiryDate,
+                      cardHolderName: cardHolderName,
+                      cvvCode: cvvCode,
+                      showBackView: false,
+                      onCreditCardWidgetChange: (CreditCardBrand) {},
+                    ),
+                  );
+                },
+              ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.pushNamed(context, NewCardPage.routeName).then((_) {
+              _loadCards();
+            });
+          },
+          child: const Icon(Icons.add),
+        ),
+      );
+    }
+    return const Text('lol');
+  }
 }
+
+
+
+
+// ToDo  - Sort out shared preferences page , name on the card, ocr button into form field 
